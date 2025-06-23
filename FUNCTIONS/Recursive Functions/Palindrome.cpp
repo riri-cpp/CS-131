031f1e48a90845eab77833cec7971a6f53ec7275
riri-cpp <pasiarheman7@gmail.com>
2025-06-23 22:16:26 +0800

    Create Palindrome.cpp

---
+++ FUNCTIONS/Recursive Functions/Palindrome.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Recursive function to check if a string is a palindrome
+bool isPalindrome(string str, int start, int end) {
+    // Base case: if we've checked all characters or reached the middle
+    if (start >= end) {
+        return true;
+    }
+    
+    // If characters at start and end positions don't match, it's not a palindrome
+    if (str[start] != str[end]) {
+        return false;
+    }
+    
+    // Recursive case: check the substring from start+1 to end-1
+    return isPalindrome(str, start + 1, end - 1);
+}
+
+int main() {
+    string input;
+    
+    cout << "Enter a string to check if it's a palindrome: ";
+    getline(cin, input);
+    
+    if (isPalindrome(input, 0, input.length() - 1)) {
+        cout << "\"" << input << "\" is a palindrome." << endl;
+    } else {
+        cout << "\"" << input << "\" is not a palindrome." << endl;
+    }
+    
+    return 0;
+}
