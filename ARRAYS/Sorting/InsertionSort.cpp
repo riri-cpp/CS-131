b059f7e7a58ff251ee366889ea004d1783fc6504
riri-cpp <pasiarheman7@gmail.com>
2025-06-23 21:45:41 +0800

    Create InsertionSort.cpp

---
+++ ARRAYS/Sorting/InsertionSort.cpp
@@ -0,0 +1,12 @@
+void insertionSort(int array[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = array[i];
+        int j = i - 1;
+        
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+}
