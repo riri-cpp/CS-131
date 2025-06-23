2c0be19d68fe39672b1cbf8431f8bd09192b0b60
riri-cpp <pasiarheman7@gmail.com>
2025-06-23 21:47:41 +0800

    Create FindMax.cpp

---
+++ ARRAYS/Algorithms/FindMax.cpp
@@ -0,0 +1,9 @@
+int findMax(int array[], int size){
+    int max = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
