70735c9d890848a9e5d73358847d3b716dc60410
riri-cpp <pasiarheman7@gmail.com>
2025-06-23 21:45:19 +0800

    Create BubbleSort.cpp

---
+++ ARRAYS/Sorting/BubbleSort.cpp
@@ -0,0 +1,12 @@
+void bubbleSort(int array[], int size){
+    int temp;
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+            if(array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
